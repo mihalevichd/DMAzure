427a8a8781163f9ccadc71837e3f679f25102bcb
mihalevichd <donny.mihalevich@wiz.io>
2026-08-21 12:02:41 -0500

    Add AKS cluster for Tasky app

--- infrastructure/azure/main.tf
+++ infrastructure/azure/main.tf
@@ -151,3 +151,26 @@ resource "azurerm_linux_virtual_machine" "mongo_vm" {
   EOF
   )
 }
+# Create the Azure Kubernetes Service (AKS) Cluster
+resource "azurerm_kubernetes_cluster" "wiz_aks" {
+  name                = "aks-wiz-exercise"
+  location            = azurerm_resource_group.wiz_rg.location
+  resource_group_name = azurerm_resource_group.wiz_rg.name
+  dns_prefix          = "wiztasky"
+
+  default_node_pool {
+    name           = "default"
+    node_count     = 1
+    vm_size        = "Standard_B2s" 
+    vnet_subnet_id = azurerm_subnet.private_subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+}
